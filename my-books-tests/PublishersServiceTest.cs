1d4a2cfc0ebe5426931bd97cbb22a1706a8f781f
Mohamad Ali Diab <m.ali.diab@outlook.com>
2022-04-19 12:42:32 +0300

    last commit

---
+++ my-books-tests/PublishersServiceTest.cs
@@ -0,0 +1,33 @@
+using firstproject.data;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace my_books_tests
+{
+    public class PublishersServiceTest
+    {
+        private static DbContextOptions<AppDbContext> dbContextOptions= new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "BookDbTest")
+            .Options;
+
+        AppDbContext context;
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            context = new AppDbContext(dbContextOptions);
+            context.Database.EnsureCreated();
+            SeedDatabase();
+        }
+
+        [OneTimeTearDown]
+        public void CleanUp()
+        {
+            context.Database.EnsureDeleted();
+        }
+
+        private void SeedDatabase()
+        {
+
+        }
+    }
+}
